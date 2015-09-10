e0ae102b9810b9ea3b29333a80474dd0f4448cda
Stephen Cleary <github.ourteddybear@xoxy.net>
2015-09-10 07:55:21 -0400

    A few more tests.

--- test/basic.js
+++ test/basic.js
@@ -1,12 +1,28 @@
 import assert from 'assert';
 import Arc from '../src/index';
 
-describe('parse', () => {
+describe('doc context', () => {
     describe('plain string', () => {
         it('should return that string as the content', () => {
             const engine = new Arc();
             const result = engine.parse('test');
             assert.equal(result.content, 'test');
+        });
+    });
+
+    describe('html string', () => {
+        it('should return full, unescaped text as the content', () => {
+            const engine = new Arc();
+            const result = engine.parse('<b>test</b>');
+            assert.equal(result.content, '<b>test</b>');
+        });
+    });
+
+    describe('quoted string', () => {
+        it('should return full, unescaped text as the content', () => {
+            const engine = new Arc();
+            const result = engine.parse('\"\'test\'\"');
+            assert.equal(result.content, '\"\'test\'\"');
         });
     });
 });
