380652b4bd218b953054f702a72eac1f4156b532
Stephen Cleary <github.ourteddybear@xoxy.net>
2015-10-06 19:30:01 -0400

    Trying random crap.

--- babelhook.js
+++ babelhook.js
@@ -1,5 +1,9 @@
-require('babel-core/register')(
-    require('semver').lt(process.versions.node, '4.0.0') ?
-        { } :
-        { babelrc: 'node4.babelrc' }
-);
+var supportES5 = require('semver').lt(process.versions.node, '4.0.0');
+
+if (supportES5) {
+    require('babel-core/polyfill');
+}
+
+var options = supportES5 ? {} : { babelrc: 'node4.babelrc' };
+
+require('babel-core/register')(options);
