8cc0a66a475dc6a784e2ce3182a6291ea2cf6a86
Stephen Cleary <github.ourteddybear@xoxy.net>
2015-10-06 19:49:46 -0400

    Undoing random changes.

--- babelhook.js
+++ babelhook.js
@@ -1,9 +1,5 @@
-var supportES5 = require('semver').lt(process.versions.node, '4.0.0');
-
-if (supportES5) {
-    require('babel-core/polyfill');
-}
-
-var options = supportES5 ? {} : { babelrc: 'node4.babelrc' };
-
-require('babel-core/register')(options);
+require('babel-core/register')(
+    require('semver').lt(process.versions.node, '4.0.0') ?
+        { } :
+        { babelrc: 'node4.babelrc' }
+);
